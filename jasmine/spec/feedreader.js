300e4c1670da018cb8eb6658cbf294b082474a64
Yousef <yousef@live.ca>
2017-01-16 00:15:32 +0000

    Added ‘Initial Entries’ and ‘New Feed Selection’ suites

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,6 +72,7 @@ $(function() {
           * clicked and does it hide when clicked again.
           */
           it ('changes visibility when the menu icon is clicked', function(){
+            //---------- define variable to aviod repetition ----------
             var checkClassList = document.body.classList;
 
             //---------- Check if menu opens when clicked ----------
@@ -86,6 +87,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -94,10 +96,21 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         // ---------- By default jasmine will wait for 5 seconds for an asynchronous spec to finish before causing a timeout failure. ----------
+         it ('ensures that the loadFeed function is called and completes its work', function(){
+            beforeEach(function(done){
+
+            });
+         });
+    });
+
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function(){
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
